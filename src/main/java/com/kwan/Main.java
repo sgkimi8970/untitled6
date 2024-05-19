9b6fea50e9c1b8738987ae6967581a57fcc72273
KimSungKwan <sgkimi8970@gmail.com>
2024-05-19 12:06:26 +0900

    Changes

--- src/main/java/com/kwan/Main.java
+++ src/main/java/com/kwan/Main.java
@@ -1,17 +1,54 @@
 package com.kwan;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class BaseballGame {
+    public void play(String masterAnswer){
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            while(true){
+                System.out.print("PlayerAnswer : ");
+                String playerAnswer = br.readLine();
+
+                String[] ma = masterAnswer.split("");
+                String[] pa = playerAnswer.split("");
+                if(result(ma, pa)){
+                    break;
+                }
+            }
         }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean result(String[] ma, String[] pa){
+        //채점시작
+        int strikeCount = 0;
+        int ballCount = 0;
+        for(int i = 0; i < ma.length; i++){
+            for(int j = 0; j < pa.length; j++){
+                if(ma[i].equals(pa[j])){
+                    if(i == j){
+                        strikeCount++;
+                    }
+                    else{
+                        ballCount++;
+                    }
+                }
+            }
+        }
+
+        //결과출력
+        if(strikeCount == 0 && ballCount == 0){
+            System.out.println("(OUTPUT) (null)");
+        }
+        else{
+            System.out.println("(OUTPUT) " + (strikeCount > 0 ? strikeCount+"S" : "") + (ballCount > 0 ? ballCount+"B" : ""));
+        }
+
+        return strikeCount == ma.length;
     }
 }
